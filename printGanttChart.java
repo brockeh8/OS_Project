a788b61fcc1ca2777daf26f0a4ad7a7b42d91af1
ggiacobbe2 <ggiacobbe13@gmail.com>
2025-03-21 19:59:23 -0400

    Update printGanttChart.java
    adding format for printing start times

--- printGanttChart.java
+++ printGanttChart.java
@@ -1,2 +1,14 @@
 private static void printGanttChart(List<Process> processes) {
   processes.sort((p1,p2) -> Integer.compare(p1.startTime, p2.startTime));
+
+  StringBuilder top = new StringBuilder();
+  StringBuilder bottom = new StringBuilder();
+
+  for (int i = 0; i < processes.size(); i++) {
+    Process p = processes.get(i);
+    top.append(str:"| P").append(p.pid).append(str:" "); // prints "| P# "
+    bottom.append(p.startTime).append(str:"   "); // prints spacing for times
+    if (i == processes.size() - 1) {
+      bottom.append(p.finishTime);
+    }
+  }
