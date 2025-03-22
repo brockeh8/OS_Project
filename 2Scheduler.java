c97b215bf6169c10d91f1bda864d4640d72d4b2f
brockeh8 <122937812+brockeh8@users.noreply.github.com>
2025-03-21 20:05:07 -0400

    Create 2Scheduler.java
    Starting the file for the main code

---
+++ 2Scheduler.java
@@ -0,0 +1,22 @@
+import java.io. *;
+import java.util.ArrayList;
+import java.util.List;
+
+public class 2Scheduler.java {
+    static class Process {
+        int pid;
+        int arrivalTime;
+        int burstTime;
+        int priority;
+        int startTime;
+        int finishTime; 
+        int waitingTime;
+        int turnaroundTime; 
+
+        Process(int pid, int arrivalTime, int burstTime, int priority) {
+            this.pid = pid; 
+            this.arrivalTime = arrivalTime;
+            this.burstTime = burstTime; 
+            this.priority = priority; 
+        }
+    }
