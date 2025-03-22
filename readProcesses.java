20c6cfbc8a91d0c168d10394fdfba67c1c77ead3
brockeh8 <122937812+brockeh8@users.noreply.github.com>
2025-03-22 15:03:49 -0400

    Update readProcesses.java
    Finished read

--- readProcesses.java
+++ readProcesses.java
@@ -17,15 +17,19 @@ private static List<Process> readProcesses(String fileNAme) {
             continue;
         }
         
-        int pid 
-        int arrival 
-        int burst
-        int priority
+        int pid = Integer.parseInt(parts[0]);
+        int arrival = Integer.parseInt(parts[1]);
+        int burst = Integer.parseInt(parts[2]);
+        int priority = Integer.parseInt(parts[3]);
 
         list.add(new Process(pid, arrival, burst, priority));
       }
   ]
   
-  catch ()
+  catch (IOException e)  {
+      system.out.println("Read error");
+  }
+  return list;
+}
         
           
